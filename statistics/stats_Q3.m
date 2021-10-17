0dd43d2495195ce80c747d82b1e8c9aee12a12b3
MarciaVaz <73110892+MarciaVaz@users.noreply.github.com>
2021-10-17 20:10:53 +0100

    Delete statistics directory

--- statistics/stats_Q3.m
+++
@@ -1,7 +0,0 @@
-clear all; close all; clc;
-
-%% Question 3: fMRI RDM stats compare different regions?
-% can we relate the behaviour and the fMRI?
-% Is the perception driven by the brain activation patterns in the different areas?
-% does behaviour correlate with the brain patterns?
-% behaviour data
