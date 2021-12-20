38a93dba6fa7a651d5bcd72b6162a4721e28bb2e
MarciaVaz <73110892+MarciaVaz@users.noreply.github.com>
2021-12-20 14:57:58 +0000

    missing stats

--- 04_final estimations/estimations/estimated fMRI semantic/fMRI_all_RDM_euclidean_estimation_semantic.m
+++ 04_final estimations/estimations/estimated fMRI semantic/fMRI_all_RDM_euclidean_estimation_semantic.m
@@ -1,32 +1,39 @@
 clear all;close all;clc;
 
-path = dir(pwd);
+%% returns the estimated RDMs names in the current directory
 file_names=dir('*_ROI_estimate_RDM_S*');
-file_names.name
+file_names.name;
 
+%% Initialyse the 15 ROIs RDMs vectors
 RDMs_euclidean_estimation_semantic = NaN(15,4186);
 RDMs_euclidean_estimation_nonnan_semantic = NaN(15,4186);
 
+%% Load ROIs names and stimuli names
 load('name_ROI.mat')
 load('stimuli_names_92.mat');
 
- for each_ROI=[1 8 12]%1:numel(file_names)
+%% create the RDMs matrices and save as an image
+ for each_ROI=1:numel(file_names) %[1 8 12]
     ROI_name = file_names(each_ROI).name
     estimated_ROI = load(file_names(each_ROI).name)
     comparisons = estimated_ROI.estimate_RDM_S;
     
-    % each row is a ROI and the columns are the rdm vector
+    % each row is a ROI and the columns are the dissimilarity distances
     if each_ROI==1
         RDMs_euclidean_estimation_semantic=comparisons;
     else
         RDMs_euclidean_estimation_semantic=cat(1,RDMs_euclidean_estimation_semantic,comparisons);
     end
     
-    nStimuli=92;
-    nComparisons = ((nStimuli^2)-nStimuli)/2; % counts only one way and not the same imgs
+    % number ofstimuli
+    nStimuli=92; 
+    % number of comparison values (not including repetition or same images)
+    nComparisons = ((nStimuli^2)-nStimuli)/2; 
 
+    % initialise RDM matrix
     rdm = NaN(92,92);
-
+    
+    % place the comparison values in the correct place in the matrix
     countComparison=0;
     for img_1 = 1:nStimuli-1
         for img_2 = img_1+1:nStimuli
@@ -37,11 +44,15 @@ load('stimuli_names_92.mat');
 
         end   
     end
+
+    % dissimilarity between same image is zero (diagonal line)
     for imag = 1:92
         rdm(imag,imag)=0;
     end
     
-    % find where the nans are
+    % there are missing values in the matrix: find the missing values index
+    % and place a value that is an average of row and column
+    % find where the nan values are
     [row column] = find(isnan(rdm)==1)
     for m=1:length(row)
         % get the average across that row and column
@@ -56,6 +67,7 @@ load('stimuli_names_92.mat');
         rdm_nonnan(row(idx),column(idx))=average_row_col(idx,1);
     end
     
+    % save the RMD without nan values
     save ([num2str(each_ROI) '_final_ROI_rdm_Semantic'], 'rdm_nonnan');
     
     % convert from matrix to vector
@@ -84,14 +96,16 @@ load('stimuli_names_92.mat');
     set(gca, 'TickLength',[0 0]);
     axis equal; axis image; 
     title(['Semantic RDM: ' table2array(name_ROI(each_ROI,2))],'fontsize',11);
-%     if each_ROI<10
-%         saveas(gcf,[ num2str(0) num2str(each_ROI) '_final_ROI_semantic.png']);
-%     else
-%         saveas(gcf,[ num2str(each_ROI) '_final_ROI_semantic.png']);
-%     end 
-%     close(f)
+    if each_ROI<10
+        imgName = ['C:\Users\Márcia Vaz\Documents\GitHub\Dissertation\04_final estimations\final images RDMs\',[num2str(0) num2str(each_ROI) '_final_ROI_semantic.png']] ;
+        saveas(gcf,imgName) ;
+    else
+        imgName = ['C:\Users\Márcia Vaz\Documents\GitHub\Dissertation\04_final estimations\final images RDMs\',[ num2str(each_ROI) '_final_ROI_semantic.png']] ;
+        saveas(gcf,imgName) ; 
+    end 
+    close(f)
     
  end
 
-% save RDMs_euclidean_estimation_semantic RDMs_euclidean_estimation_semantic
-% save RDMs_euclidean_estimation_nonnan_semantic RDMs_euclidean_estimation_nonnan_semantic
+save RDMs_euclidean_estimation_semantic RDMs_euclidean_estimation_semantic
+save RDMs_euclidean_estimation_nonnan_semantic RDMs_euclidean_estimation_nonnan_semantic
